dd2c12032662a4a56cb69d49393686d7177eb264
Olah Geza <olahgeza@Olahs-MacBook-Pro.local>
2024-04-13 20:34:12 +0200

    comit07

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: false,
   },
   env: {
     API_URL: "https://bcmunka01.vercel.app", 

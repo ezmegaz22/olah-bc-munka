a4f8e804dfe5ce4ebb3212d98b30a165db4c6107
Olah Geza <olahgeza@Olahs-MacBook-Pro.local>
2024-04-14 20:30:09 +0200

    asdadas

--- app/admin/products/[id]/page.jsx
+++ app/admin/products/[id]/page.jsx
@@ -1,39 +1,17 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
+
 import UpdateProduct from "@/components/admin/UpdateProduct";
 
 const getProduct = async (id) => {
-  try {
-    const { data } = await axios.get(
-      `${process.env.API_URL}/api/products/${id}`
-    );
-    return data;
-  } catch (error) {
-    console.error("Error fetching product:", error);
-    return null;
-  }
+  const { data } = await axios.get(`${process.env.API_URL}/api/products/${id}`);
+  return data;
 };
 
-const HomePage = ({ params }) => {
-  const [productData, setProductData] = useState(null);
+const HomePage = async ({ params }) => {
+  const data = await getProduct(params.id);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getProduct(params.id);
-      if (data) {
-        setProductData(data.product);
-      }
-    };
-    fetchData();
-  }, [params.id]);
-
-  if (!productData) {
-    // Itt kezelheted az adatok betöltésének állapotát, pl. loading ikon vagy üzenet
-    return <p>Loading...</p>;
-  }
-
-  return <UpdateProduct data={productData} />;
+  return <UpdateProduct data={data.product} />;
 };
 
 export default HomePage;
--- app/admin/products/page.jsx
+++ app/admin/products/page.jsx
@@ -1,44 +1,26 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
-import Products from "@/components/admin/Products";
+
 import queryString from "query-string";
+import Products from "@/components/admin/Products";
 
 const getProducts = async (searchParams) => {
-  try {
-    const urlParams = {
-      page: searchParams.page,
-    };
-    const searchQuery = queryString.stringify(urlParams);
-    const { data } = await axios.get(
-      `${process.env.API_URL}/api/products?${searchQuery}`
-    );
-    return data;
-  } catch (error) {
-    console.error("Error fetching products:", error);
-    return null;
-  }
+  const urlParams = {
+    page: searchParams.page,
+  };
+
+  const searchQuery = queryString.stringify(urlParams);
+
+  const { data } = await axios.get(
+    `${process.env.API_URL}/api/products?${searchQuery}`
+  );
+  return data;
 };
 
-const HomePage = ({ searchParams }) => {
-  const [productsData, setProductsData] = useState(null);
+const HomePage = async ({ searchParams }) => {
+  const data = await getProducts(searchParams);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getProducts(searchParams);
-      if (data) {
-        setProductsData(data);
-      }
-    };
-    fetchData();
-  }, [searchParams]);
-
-  if (!productsData) {
-    // betoltes alapot
-    return <p>Loading...</p>;
-  }
-
-  return <Products data={productsData} />;
+  return <Products data={data} />;
 };
 
 export default HomePage;

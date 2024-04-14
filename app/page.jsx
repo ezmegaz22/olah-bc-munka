c5705f2788002624adcace9e6efa835e7b70f403
Olah Geza <olahgeza@Olahs-MacBook-Pro.local>
2024-04-14 19:19:40 +0200

    asfsdgeghqerge1234124

--- app/page.jsx
+++ app/page.jsx
@@ -4,28 +4,51 @@ import ListProducts from "@/components/products/ListProducts";
 
 import queryString from "query-string";
 
-const getProducts = async (searchParams) => {
-  const urlParams = {
-    keyword: searchParams.keyword,
-    page: searchParams.page,
-    category: searchParams.category,
-    "price[gte]": searchParams.min,
-    "price[lte]": searchParams.max,
-    "ratings[gte]": searchParams.ratings,
-  };
+// const getProducts = async (searchParams) => {
+//   const urlParams = {
+//     keyword: searchParams.keyword,
+//     page: searchParams.page,
+//     category: searchParams.category,
+//     "price[gte]": searchParams.min,
+//     "price[lte]": searchParams.max,
+//     "ratings[gte]": searchParams.ratings,
+//   };
 
-  const searchQuery = queryString.stringify(urlParams);
+//   const searchQuery = queryString.stringify(urlParams);
 
-  const { data } = await axios.get(
-    `${process.env.API_URL}/api/products?${searchQuery}`
+//   const { data } = await axios.get(
+//     `${process.env.API_URL}/api/products?${searchQuery}`
+//   );
+//   return data;
+// };
+
+const Page = async ({ searchParams }) => {
+  //const productsData = await getProducts(searchParams);
+
+  return (
+    <ListProducts
+      data={{
+        resPerPage: 1,
+        productsCount: 1,
+        filteredProductsCount: 1,
+        products: [
+          {
+            _id: "asdasdas",
+            name: "asdasdas",
+            description: "asdsadsadasa",
+            price: 145,
+            category: "Monitor",
+            seller: "test",
+            stock: 345,
+            ratings: 3,
+            user: "asdasdsadasdasdas312312",
+            images: [],
+            reviews: [],
+          },
+        ],
+      }}
+    />
   );
-  return data;
 };
 
-const HomePage = async ({ searchParams }) => {
-  const productsData = await getProducts(searchParams);
-
-  return <ListProducts data={productsData} />;
-};
-
-export default HomePage;
+export default Page;

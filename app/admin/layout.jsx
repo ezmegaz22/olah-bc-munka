f2a0d1cd32d5adb3be8ba73eaa038489b151ec10
Olah Geza <olahgeza@Olahs-MacBook-Pro.local>
2024-04-14 09:20:56 +0200

    verceltest

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -5,7 +5,7 @@ export default function AdminLayout({ children }) {
     <>
       <section className="py-5 sm:py-7 bg-blue-100">
         <div className="container max-w-screen-xl mx-auto px-4">
-          <h1 className="text-bold text-2xl">Admin Dashboard</h1>
+          <h1 className="text-bold text-2xl">Adminn Dashboard</h1>
         </div>
       </section>
 

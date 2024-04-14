a92e65987a9c998e99613e4445b3aa9ba4e9feaa
Olah Geza <olahgeza@Olahs-MacBook-Pro.local>
2024-04-14 14:14:24 +0200

    asdada

--- components/admin/UpdateUser.jsx
+++ components/admin/UpdateUser.jsx
@@ -97,7 +97,7 @@ const UpdateUser = ({ user }) => {
           type="submit"
           className="my-2 px-4 py-2 text-center w-full inline-block text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700"
         >
-          Frissités
+          Frissitéss
         </button>
       </form>
     </div>

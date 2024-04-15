3fc6ad42cc57d040392d55c19224dd039e1638f3
Olah Geza <olahgeza@Olahs-MacBook-Pro.local>
2024-04-15 08:08:01 +0200

    asdasdddd

--- app/me/page.jsx
+++ app/me/page.jsx
@@ -14,7 +14,7 @@ const getAddresses = async () => {
 
   const { data } = await axios.get(`${process.env.API_URL}/api/address`, {
     headers: {
-      Cookie: `${cookieName}=${nextAuthSessionToken}`,
+      Cookie: `${"__Secure-next-auth.session-token"}=${"eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..ISf0mARG941owisX.Nl-jDSqPJEsPAIOwiFXr8Txdlliv2-egdkjJGWLkuhQybqjLNnd-gaCHap5RLnFU6x1Xz0DK5aBaGEVwsdL5ASZNCM2XjSlcjno4Bzu7XjCqJSCycRqhV9ukLbiNc4uLCtDmZGFpsTSBMllRccCTmkn-lU9u1eskukoUlx2dSM3QBOL_TSwPck4bO0bA4_F3vlnoBPfo1s7ciShJwMUAGNvX-qYCKl547fF03-VLgPk3b2i92C-144tUylKBTrjjUDXn42Etgj1Bqm7lD7zEGwF9AMogeUXUBQZNpnuoRiXzz6X6Lr_8GJ3CZI7n2frxzdOiPfkUkTz272sTNJxAK7sVtrJEHvMMaUZzh2aeeCYDMXM0ZIz95S0gQpHBipYwrXs.y4fu5rO_N8WwXr9Xtj84hg"}`,
     },
   });
 
